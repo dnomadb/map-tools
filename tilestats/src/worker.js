2c5dfea0708de076289d542c0ec5a4b748f978eb
Damon <nomad.damon@gmail.com>
2022-05-26 14:59:39 -0700

    add worker

--- tilestats/src/worker.js
+++ tilestats/src/worker.js
@@ -1,5 +1,5 @@
 const VectorTileLayer = require("@mapbox/vector-tile/lib/vectortilelayer");
-const Protobuf = require('pbf');
+const Protobuf = require("pbf");
 
 function VectorTile(pbf, end) {
   this.layers = pbf.readFields(readTile, { last: 0 }, end);
@@ -15,30 +15,34 @@ const readTile = (tag, layers, pbf) => {
   }
 };
 
-const tileHash = {}
+const tileHash = {};
 
-onmessage = function(e) {
-    const tileInfo = {layers: {}};
-    if (!(e.data in tileHash)) {
-      fetch(e.data).then((response) => {
-          return response.arrayBuffer();
-        }).then((data) => {
-          tileHash[e.data] = true
-          const tile = new VectorTile(new Protobuf(data));
-          tileInfo.size = data.byteLength / 1000
-          for (let layer in tile.layers) {
+onmessage = function (e) {
+  const tileInfo = { layers: {} };
+  if (!(e.data in tileHash)) {
+    fetch(e.data)
+      .then((response) => {
+        return response.arrayBuffer();
+      })
+      .then((data) => {
+        tileHash[e.data] = true;
+        const tile = new VectorTile(new Protobuf(data));
+        tileInfo.size = data.byteLength / 1000;
+        for (let layer in tile.layers) {
           const layerInfo = {
             features: [tile.layers[layer].length],
             coordinates: [0],
-            kb: [tile.layers[layer].bytelength / 1000]
+            kb: [tile.layers[layer].bytelength / 1000],
           };
           const layerPropertyHasher = [];
-            for (var f = 0; f < tile.layers[layer].length; f++) {
-                for (const [key, value] of Object.entries(tile.layers[layer].feature(f).properties)) {
-                  layerPropertyHasher.push(`${key}:${value}`);
-                }
-              layerInfo.features[0] += tile.layers[layer].length;
-              let coordinates;
+          for (var f = 0; f < tile.layers[layer].length; f++) {
+            for (const [key, value] of Object.entries(
+              tile.layers[layer].feature(f).properties
+            )) {
+              layerPropertyHasher.push(`${key}:${value}`);
+            }
+            layerInfo.features[0] += tile.layers[layer].length;
+            let coordinates;
 
             let geometry = tile.layers[layer]
               .feature(f)
@@ -64,14 +68,14 @@ onmessage = function(e) {
                 layerInfo.coordinates[0] += part.length;
               });
             });
-            }
-            const unique = [ ...new Set(layerPropertyHasher)];
-            layerInfo.unique_properties = [unique.length];
-            tileInfo.layers[layer] = layerInfo;
           }
-          postMessage(tileInfo);
-        });
-    } else {
-      console.log(`Already checked ${e.data}, skipping`)
-    }
-} 
+          const unique = [...new Set(layerPropertyHasher)];
+          layerInfo.unique_properties = [unique.length];
+          tileInfo.layers[layer] = layerInfo;
+        }
+        postMessage(tileInfo);
+      });
+  } else {
+    console.log(`Already checked ${e.data}, skipping`);
+  }
+};
