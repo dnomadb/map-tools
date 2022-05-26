6d95e213c2641ebf28f61c15961239c60b46304b
Damon <nomad.damon@gmail.com>
2022-05-26 14:59:14 -0700

    add worker

---
+++ tilestats/dist/src_worker_js.main.js
@@ -0,0 +1,207 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/worker.js":
+/*!***********************!*\
+  !*** ./src/worker.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const VectorTile = (__webpack_require__(/*! @mapbox/vector-tile */ \"./node_modules/@mapbox/vector-tile/index.js\").VectorTile);\nconst Protobuf = __webpack_require__(/*! pbf */ \"./node_modules/pbf/index.js\");\n\n\nonmessage = function(e) {\n    const zxy = /(\\d+)\\/(\\d+)\\/(\\d+)\\./.exec(e.data).slice(1, 5).map((t) => {\n        return parseInt(t);\n      });\n    const tileInfo = {layers: {}};\n    fetch(e.data).then((response) => {\n        return response.arrayBuffer();\n      }).then((data) => {\n        const tile = new VectorTile(new Protobuf(data));\n        tileInfo.size = data.length;\n        for (let layer in tile.layers) {\n        const layerInfo = {features: [0], coordinates: [0]};\n        const layerPropertyHasher = [];\n        \n          for (var f = 0; f < tile.layers[layer].length; f++) {\n              for (const [key, value] of Object.entries(tile.layers[layer].feature(f).properties)) {\n                layerPropertyHasher.push(`${key}:${value}`);\n              }\n            layerInfo.features[0] += tile.layers[layer].length;\n            layerInfo.coordinates[0] += tile.layers[layer].feature(f).loadGeometry().length;\n          }\n          const unique = [ ...new Set(layerPropertyHasher)];\n          layerInfo.unique_properties = [unique.length];\n          tileInfo.layers[layer] = layerInfo;\n        }\n\n        postMessage(tileInfo);\n      });\n} \n\n//# sourceURL=webpack://tilestats/./src/worker.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/******/ 	// the startup function
+/******/ 	__webpack_require__.x = () => {
+/******/ 		// Load entry module and return exports
+/******/ 		// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 		var __webpack_exports__ = __webpack_require__.O(undefined, ["vendors-node_modules_mapbox_vector-tile_index_js-node_modules_pbf_index_js"], () => (__webpack_require__("./src/worker.js")))
+/******/ 		__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 		return __webpack_exports__;
+/******/ 	};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	(() => {
+/******/ 		__webpack_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__webpack_require__.e = (chunkId) => {
+/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
+/******/ 				__webpack_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	(() => {
+/******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
+/******/ 		__webpack_require__.u = (chunkId) => {
+/******/ 			// return url for filenames based on template
+/******/ 			return "" + chunkId + ".main.js";
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/importScripts chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded chunks
+/******/ 		// "1" means "already loaded"
+/******/ 		var installedChunks = {
+/******/ 			"src_worker_js": 1
+/******/ 		};
+/******/ 		
+/******/ 		// importScripts chunk loading
+/******/ 		var installChunk = (data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			while(chunkIds.length)
+/******/ 				installedChunks[chunkIds.pop()] = 1;
+/******/ 			parentChunkLoadingFunction(data);
+/******/ 		};
+/******/ 		__webpack_require__.f.i = (chunkId, promises) => {
+/******/ 			// "1" is the signal for "already loaded"
+/******/ 			if(!installedChunks[chunkId]) {
+/******/ 				if(true) { // all chunks have JS
+/******/ 					importScripts(__webpack_require__.p + __webpack_require__.u(chunkId));
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunktilestats"] = self["webpackChunktilestats"] || [];
+/******/ 		var parentChunkLoadingFunction = chunkLoadingGlobal.push.bind(chunkLoadingGlobal);
+/******/ 		chunkLoadingGlobal.push = installChunk;
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/startup chunk dependencies */
+/******/ 	(() => {
+/******/ 		var next = __webpack_require__.x;
+/******/ 		__webpack_require__.x = () => {
+/******/ 			return __webpack_require__.e("vendors-node_modules_mapbox_vector-tile_index_js-node_modules_pbf_index_js").then(next);
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// run startup
+/******/ 	var __webpack_exports__ = __webpack_require__.x();
+/******/ 	
+/******/ })()
+;
---
+++ tilestats/src/worker.js
@@ -0,0 +1,77 @@
+const VectorTileLayer = require("@mapbox/vector-tile/lib/vectortilelayer");
+const Protobuf = require('pbf');
+
+function VectorTile(pbf, end) {
+  this.layers = pbf.readFields(readTile, { last: 0 }, end);
+  delete this.layers.last;
+}
+
+const readTile = (tag, layers, pbf) => {
+  if (tag === 3) {
+    const layer = new VectorTileLayer(pbf, pbf.readVarint() + pbf.pos);
+    layer.bytelength = pbf.pos - layers.last;
+    layers.last = pbf.pos;
+    if (layer.length) layers[layer.name] = layer;
+  }
+};
+
+const tileHash = {}
+
+onmessage = function(e) {
+    const tileInfo = {layers: {}};
+    if (!(e.data in tileHash)) {
+      fetch(e.data).then((response) => {
+          return response.arrayBuffer();
+        }).then((data) => {
+          tileHash[e.data] = true
+          const tile = new VectorTile(new Protobuf(data));
+          tileInfo.size = data.byteLength / 1000
+          for (let layer in tile.layers) {
+          const layerInfo = {
+            features: [tile.layers[layer].length],
+            coordinates: [0],
+            kb: [tile.layers[layer].bytelength / 1000]
+          };
+          const layerPropertyHasher = [];
+            for (var f = 0; f < tile.layers[layer].length; f++) {
+                for (const [key, value] of Object.entries(tile.layers[layer].feature(f).properties)) {
+                  layerPropertyHasher.push(`${key}:${value}`);
+                }
+              layerInfo.features[0] += tile.layers[layer].length;
+              let coordinates;
+
+            let geometry = tile.layers[layer]
+              .feature(f)
+              .toGeoJSON(10, 10, 10).geometry;
+            if (geometry["type"] === "Point") {
+              coordinates = [[[geometry["coordinates"]]]];
+            } else if (
+              geometry["type"] === "MultiPoint" ||
+              geometry["type"] === "LineString"
+            ) {
+              coordinates = [[geometry["coordinates"]]];
+            } else if (
+              geometry["type"] === "MultiLineString" ||
+              geometry["type"] === "Polygon"
+            ) {
+              coordinates = [geometry["coordinates"]];
+            } else {
+              coordinates = geometry["coordinates"];
+            }
+
+            coordinates.forEach((chunk) => {
+              chunk.forEach((part) => {
+                layerInfo.coordinates[0] += part.length;
+              });
+            });
+            }
+            const unique = [ ...new Set(layerPropertyHasher)];
+            layerInfo.unique_properties = [unique.length];
+            tileInfo.layers[layer] = layerInfo;
+          }
+          postMessage(tileInfo);
+        });
+    } else {
+      console.log(`Already checked ${e.data}, skipping`)
+    }
+} 
